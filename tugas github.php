beb23bdca3a5539a12e73279e54d87561db26cc6
DevaAKhan <55737290+DevaAKhan@users.noreply.github.com>
2020-04-09 14:05:48 +0700

    Update tugas github.php

--- tugas github.php
+++ tugas github.php
@@ -2,5 +2,6 @@
 echo 'Nama: Deva Abel Khan <br>' ;
 echo 'Kelas: XI RPL 1 <br>' ;
 echo 'Asal Sekolah : Smk Taruna Bhakti Depok <br>' ;
-echo 'No.hp: 081295446977' ;
+echo 'No.hp: 081295446977 <br>' ;
+echo 'Email : devaabel207@gmail.com <br>' ;
 ?>
